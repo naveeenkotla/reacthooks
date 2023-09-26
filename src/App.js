79b8e9642ded27b812422a1510c0a345bd652703
naveeen kotla <naveenkotla3232@gmail.com>
2023-09-26 22:54:29 +0530

    adding all the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  let [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="newww">
+    <div><h1>press to increase or decrese</h1></div>
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count + 1)}>
+        up
+      </button>
+      <hr></hr>
+      <button onClick={() => setCount(count - 1)}>
+        down
+      </button>
     </div>
+    </>
   );
 }
 
